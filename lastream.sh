dd52fa273e6085660d13ff0e62553299b9efd400
cutesnake <lijunting@Cutesnake-MacBook-Pro.local>
2021-05-21 13:51:03 +0800

    move server

---
+++ lastream.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while true
+do
+    lofigirl="$(youtube-dl -g https://www.youtube.com/watch?v=5qap5aO4i9A)"
+    youtube="$(/usr/local/ffmpeg4/bin/ffmpeg -re -i ${lofigirl} -vcodec copy -acodec copy -f flv  -flvflags no_duration_filesize "rtmp://1.15.170.82/testmusic")"
+    echo "${youtube}"
+done
