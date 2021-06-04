2499d40fb80e56862cd17ff31eb40400531bb682
suchacutesnake <63144725+suchacutesnake@users.noreply.github.com>
2021-06-05 01:23:51 +0800

    Update lastream.sh

--- lastream.sh
+++ lastream.sh
@@ -4,5 +4,5 @@ do
     lofigirl="$(youtube-dl -g https://www.youtube.com/watch?v=5qap5aO4i9A)"
     youtube="$(/usr/local/ffmpeg4/bin/ffmpeg -re -i ${lofigirl} -vcodec copy -acodec copy -f flv  -flvflags no_duration_filesize "rtmp://1.15.170.82/testmusic")"
     echo "${youtube}"
-    sleep 15
+    sleep 8
 done
