f06405a324e3d3f7580606cadf7fe53950c1e6d0
suchacutesnake <63144725+suchacutesnake@users.noreply.github.com>
2021-10-07 14:06:24 +0800

    Update lofigirl.sh

--- lofigirl.sh
+++ lofigirl.sh
@@ -2,6 +2,7 @@
 while true
 do
     lofigirl="$(youtube-dl -g https://www.youtube.com/watch?v=5qap5aO4i9A)"
-    youtube="$(ffmpeg -re -i ${lofigirl} -vcodec copy -acodec copy -f flv  -flvflags no_duration_filesize "rtmp://1.15.170.82/testmusic")"
+    youtube="$(ffmpeg -re -i ${lofigirl} -vcodec copy -acodec copy -bsf:a aac_adtstoasc -f flv -flvflags no_duration_filesize "rtmp://66.42.100.165:1935/live/livestream.m3u8")"
     echo "${youtube}"
+    sleep 10
 done
